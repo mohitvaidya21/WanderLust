5dc5f7bbc8f5e5ba2c5e3707677eedc79f32a82b
mohitvaidya21 <vaidyamohit388@gmail.com>
2026-05-28 23:24:24 +0530

    Add project files

--- index.js
+++ index.js
@@ -82,6 +82,9 @@ app.use((req,res,next)=>{
     next();
 })
 
+app.get("/", (req, res) => {
+    res.redirect("/listings");
+});
 app.use("/",userRouter);
 app.use("/listings",listingRouter);
 app.use("/listings/:id/reviews",reviewRouter);
